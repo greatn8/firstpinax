b10ad77d0fab862e13d6cbe46be9ee1b82d71e03
greatn8 <greatn8@users.noreply.github.com>
2017-09-14 23:47:43 +1000

    index.html had 3 graphs 3 different graphs from one set of data and a 3rd from a different set of data
    charts.html has original 2

--- newsite/views.py
+++ newsite/views.py
@@ -44,12 +44,23 @@ class ChartData(APIView):
 	def get(self, request, format=None):
 		#provide label data for label field below
 		QLD_count = Project.objects.all().filter(Project_State="QLD").count()
+		ProjectsQLDJANstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-02-01', PROJ_START_DATE_DETAIL__gte='2017-01-01',Project_State="QLD").count(),
+
+		ProjectsQLDFEBstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-03-01', PROJ_START_DATE_DETAIL__gte='2017-02-01',Project_State="QLD").count(),
+		ProjectsQLDMARstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-04-01', PROJ_START_DATE_DETAIL__gte='2017-03-01',Project_State="QLD").count(),
+
 		labels = ["QLD Projects", "Blue", "Yellow", "Green", "Purple", "Orange"]
 		default_items = [QLD_count, 500, 654, 343, 455, 600 ]
+
+		labelsqldcom17 = ["Jan", "Feb", "March"]
+		QLDCommence17 = [100, 200, 300]
 		#data to return in json format
 		data = {
 			"labels": labels,
 			"default": default_items,
+
+			"QLDCommence17": QLDCommence17,
+			"labelsqldcom17": labelsqldcom17,
 			#gets count of pbject in Project model
 			"projects": Project.objects.count(),
 			#count all projecst in QLD
@@ -72,52 +83,52 @@ class ChartData(APIView):
 			"ProjectsQLDDECstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2018-01-01', PROJ_START_DATE_DETAIL__gte='2017-12-01',Project_State="QLD").count(),
 		}	
 		return Response(data)
-#QLD Commencemnt Chart
-class Chart2(APIView):
-	authentication_classes = []
-	permission_classes = []
+# QLD Commencemnt Chart
+# class Chart2(APIView):
+# 	authentication_classes = []
+# 	permission_classes = []
 
-	def get(self, request, format=None):
-		#provide label data for label field below
-		ProjectsQLDJANstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-02-01', PROJ_START_DATE_DETAIL__gte='2017-01-01',Project_State="QLD").count(),
+# 	def get(self, request, format=None):
+# 		#provide label data for label field below
+# 		ProjectsQLDJANstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-02-01', PROJ_START_DATE_DETAIL__gte='2017-01-01',Project_State="QLD").count(),
 
-		ProjectsQLDFEBstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-03-01', PROJ_START_DATE_DETAIL__gte='2017-02-01',Project_State="QLD").count(),
-		ProjectsQLDMARstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-04-01', PROJ_START_DATE_DETAIL__gte='2017-03-01',Project_State="QLD").count(),
-		ProjectsQLDAPRstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-05-01', PROJ_START_DATE_DETAIL__gte='2017-04-01',Project_State="QLD").count(),
-		ProjectsQLDMAYstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-06-01', PROJ_START_DATE_DETAIL__gte='2017-05-01',Project_State="QLD").count(),
-		ProjectsQLDJUNstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-07-01', PROJ_START_DATE_DETAIL__gte='2017-06-01',Project_State="QLD").count(),
-		ProjectsQLDJULstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-08-01', PROJ_START_DATE_DETAIL__gte='2017-07-01',Project_State="QLD").count(),
-		ProjectsQLDAUGstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-09-01', PROJ_START_DATE_DETAIL__gte='2017-08-01',Project_State="QLD").count(),
-		ProjectsQLDSEPstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-10-01', PROJ_START_DATE_DETAIL__gte='2017-09-01',Project_State="QLD").count(),
-		ProjectsQLDOCTstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-11-01', PROJ_START_DATE_DETAIL__gte='2017-10-01',Project_State="QLD").count(),
-		ProjectsQLDNOVstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-12-01', PROJ_START_DATE_DETAIL__gte='2017-11-01',Project_State="QLD").count(),
-		ProjectsQLDDECstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2018-01-01', PROJ_START_DATE_DETAIL__gte='2017-12-01',Project_State="QLD").count(),
+# 		ProjectsQLDFEBstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-03-01', PROJ_START_DATE_DETAIL__gte='2017-02-01',Project_State="QLD").count(),
+# 		ProjectsQLDMARstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-04-01', PROJ_START_DATE_DETAIL__gte='2017-03-01',Project_State="QLD").count(),
+# 		ProjectsQLDAPRstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-05-01', PROJ_START_DATE_DETAIL__gte='2017-04-01',Project_State="QLD").count(),
+# 		ProjectsQLDMAYstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-06-01', PROJ_START_DATE_DETAIL__gte='2017-05-01',Project_State="QLD").count(),
+# 		ProjectsQLDJUNstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-07-01', PROJ_START_DATE_DETAIL__gte='2017-06-01',Project_State="QLD").count(),
+# 		ProjectsQLDJULstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-08-01', PROJ_START_DATE_DETAIL__gte='2017-07-01',Project_State="QLD").count(),
+# 		ProjectsQLDAUGstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-09-01', PROJ_START_DATE_DETAIL__gte='2017-08-01',Project_State="QLD").count(),
+# 		ProjectsQLDSEPstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-10-01', PROJ_START_DATE_DETAIL__gte='2017-09-01',Project_State="QLD").count(),
+# 		ProjectsQLDOCTstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-11-01', PROJ_START_DATE_DETAIL__gte='2017-10-01',Project_State="QLD").count(),
+# 		ProjectsQLDNOVstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-12-01', PROJ_START_DATE_DETAIL__gte='2017-11-01',Project_State="QLD").count(),
+# 		ProjectsQLDDECstart = Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2018-01-01', PROJ_START_DATE_DETAIL__gte='2017-12-01',Project_State="QLD").count(),
 
-		labels = ["QLD Projects", "Blue", "Yellow", "Green", "Purple", "Orange"]
-		default_items = [ProjectsQLDJANstart, ProjectsQLDFEBstart, ProjectsQLDMARstart ]
-		#data to return in json format
-		data = {
-			"labels": labels,
-			"default": default_items,
-			#gets count of pbject in Project model
-			"projects": Project.objects.count(),
-			#count all projecst in QLD
-			"projectsQLD": Project.objects.all().filter(Project_State="QLD").count(),
-			#count all projects in NSW
-			"projectsNSW": Project.objects.all().filter(Project_State="NSW").count(),
-			"projectsVIC": Project.objects.all().filter(Project_State="VIC").count(),
-			#projectsstarting month and state can be modified with more filters yyyy-mm-dd - this QLD JAN commencemnt
-			"ProjectsQLDJANstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-02-01', PROJ_START_DATE_DETAIL__gte='2017-01-01',Project_State="QLD").count(),
-			"ProjectsQLDJFEBstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-03-01', PROJ_START_DATE_DETAIL__gte='2017-02-01',Project_State="QLD").count(),
-			"ProjectsQLDMARstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-04-01', PROJ_START_DATE_DETAIL__gte='2017-03-01',Project_State="QLD").count(),
-			"ProjectsQLDAPRstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-05-01', PROJ_START_DATE_DETAIL__gte='2017-04-01',Project_State="QLD").count(),
-			"ProjectsQLDMAYstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-06-01', PROJ_START_DATE_DETAIL__gte='2017-05-01',Project_State="QLD").count(),
-			"ProjectsQLDJUNstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-07-01', PROJ_START_DATE_DETAIL__gte='2017-06-01',Project_State="QLD").count(),
-			"ProjectsQLDJULstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-08-01', PROJ_START_DATE_DETAIL__gte='2017-07-01',Project_State="QLD").count(),
-			"ProjectsQLDAUGstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-09-01', PROJ_START_DATE_DETAIL__gte='2017-08-01',Project_State="QLD").count(),
-			"ProjectsQLDSEPstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-10-01', PROJ_START_DATE_DETAIL__gte='2017-09-01',Project_State="QLD").count(),
-			"ProjectsQLDOCTstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-11-01', PROJ_START_DATE_DETAIL__gte='2017-10-01',Project_State="QLD").count(),
-			"ProjectsQLDNOVstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-12-01', PROJ_START_DATE_DETAIL__gte='2017-11-01',Project_State="QLD").count(),
-			"ProjectsQLDDECstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2018-01-01', PROJ_START_DATE_DETAIL__gte='2017-12-01',Project_State="QLD").count(),
-		}	
-		return Response(data)		
+# 		labels = ["QLD Projects", "Blue", "Yellow", "Green", "Purple", "Orange"]
+# 		default_items = [ProjectsQLDJANstart, ProjectsQLDFEBstart, ProjectsQLDMARstart ]
+# 		#data to return in json format
+# 		data = {
+# 			"labels": labels,
+# 			"default": default_items,
+# 			#gets count of pbject in Project model
+# 			"projects": Project.objects.count(),
+# 			#count all projecst in QLD
+# 			"projectsQLD": Project.objects.all().filter(Project_State="QLD").count(),
+# 			#count all projects in NSW
+# 			"projectsNSW": Project.objects.all().filter(Project_State="NSW").count(),
+# 			"projectsVIC": Project.objects.all().filter(Project_State="VIC").count(),
+# 			#projectsstarting month and state can be modified with more filters yyyy-mm-dd - this QLD JAN commencemnt
+# 			"ProjectsQLDJANstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-02-01', PROJ_START_DATE_DETAIL__gte='2017-01-01',Project_State="QLD").count(),
+# 			"ProjectsQLDJFEBstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-03-01', PROJ_START_DATE_DETAIL__gte='2017-02-01',Project_State="QLD").count(),
+# 			"ProjectsQLDMARstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-04-01', PROJ_START_DATE_DETAIL__gte='2017-03-01',Project_State="QLD").count(),
+# 			"ProjectsQLDAPRstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-05-01', PROJ_START_DATE_DETAIL__gte='2017-04-01',Project_State="QLD").count(),
+# 			"ProjectsQLDMAYstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-06-01', PROJ_START_DATE_DETAIL__gte='2017-05-01',Project_State="QLD").count(),
+# 			"ProjectsQLDJUNstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-07-01', PROJ_START_DATE_DETAIL__gte='2017-06-01',Project_State="QLD").count(),
+# 			"ProjectsQLDJULstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-08-01', PROJ_START_DATE_DETAIL__gte='2017-07-01',Project_State="QLD").count(),
+# 			"ProjectsQLDAUGstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-09-01', PROJ_START_DATE_DETAIL__gte='2017-08-01',Project_State="QLD").count(),
+# 			"ProjectsQLDSEPstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-10-01', PROJ_START_DATE_DETAIL__gte='2017-09-01',Project_State="QLD").count(),
+# 			"ProjectsQLDOCTstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-11-01', PROJ_START_DATE_DETAIL__gte='2017-10-01',Project_State="QLD").count(),
+# 			"ProjectsQLDNOVstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2017-12-01', PROJ_START_DATE_DETAIL__gte='2017-11-01',Project_State="QLD").count(),
+# 			"ProjectsQLDDECstart": Project.objects.filter(PROJ_START_DATE_DETAIL__lt='2018-01-01', PROJ_START_DATE_DETAIL__gte='2017-12-01',Project_State="QLD").count(),
+# 		}	
+# 		return Response(data)		
